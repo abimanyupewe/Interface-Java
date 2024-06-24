d779461853e3457cc71c93db52929894fdd2df5f
A-11 <A-11@A-11>
2024-06-24 10:47:11 +0700

    pertemuan 11

---
+++ Interface_java_pert11/src/main/java/interface_java_pert11/Kendaraan.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interface_java_pert11;
+
+/**
+ *
+ * @author A-11
+ */
+public interface Kendaraan {
+    void info();
+}
