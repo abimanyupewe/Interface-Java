f0944c92591edf3b65e3d602370ef194ad0a5530
ASUS <ASUS@vivobook.mshome.net>
2024-06-26 11:59:54 +0700

    gabungkan

---
+++ Interface_java_pert11/src/main/java/Implementasi_DataBase_Interface/MemoryStorage.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Implementasi_DataBase_Interface;
+
+/**
+ *
+ * @author A-11
+ */
+public class MemoryStorage implements DataStorege{
+
+    private String data;
+
+    @Override
+    public void writeData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String readData() {
+        return this.data;
+    }
+
+}
